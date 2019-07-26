7b731f7564c70928caadc0154549f3e7c82e44fe
Александр <ussov39@gmail.com>
2019-07-26 10:02:20 +0300

    Finished my hometask

---
+++ HomeWork/Task_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Усов Александр
+ * 5.
+а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+б) Сделать задание, только вывод организуйте в центре экрана
+в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y)*/
+
+namespace Task_5
+{
+    class Program
+    {
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+        static void print(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+        }
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(50, 20);
+            Console.SetBufferSize(50, 20);
+            /* Пятое задание*/
+
+/*Пункт "a"*/
+string fullName = "Усов Александр, Москва";
+            Console.WriteLine(fullName);
+
+            ///*Пункт "б"*/
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine(fullName);
+
+            /*Пункт "c"*/
+
+            print(fullName, Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Pause();
+        }
+    }
+}
---
+++ HomeWork/Task_four/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+/* Усов Александр
+ * 4. Написать программу обмена значениями двух переменных.
+а) с использованием третьей переменной;
+б) *без использования третьей переменной*/
+namespace Task_four
+{
+    class Program
+    {
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+        static void Main()
+        {
+
+            int a, b, c;
+          
+
+            /*Четвёртое задание*/
+
+            /*Пункт "a"*/
+            a = 1;
+            b = 2;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"a={a}; b = {b}");
+
+            /* Пункт "b"*/
+            a = 1;
+            b = 2;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"a={a}; b = {b}");
+            Pause();
+        }
+    }
+}
